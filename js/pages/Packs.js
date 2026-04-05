b8c0b4672e0e09b5a82a79e7b2dcd82220f6bff7
scruffiebaka <itsmew2468@gmail.com>
2026-04-05 16:36:18 +0530

    hj

--- js/pages/Packs.js
+++ js/pages/Packs.js
@@ -1,95 +1,60 @@
-import { fetchPacks } from "../content.js";
-
-import Spinner from "../components/Spinner.js";
-
-export default {
-    components: { Spinner },
-
-    data: () => ({
-        packs: [],
-        selectedPack: 0,
-        selectedLevel: 0,
-        loading: true,
-        err: null
-    }),
-
-    template: `
-        <main v-if="loading">
-            <Spinner></Spinner>
-        </main>
-
-        <main v-else class="page-list page-packs-container">
-            <div class="page-packs">
-
-                <div class="packs-container">
-                    <table class="list">
-                        <tr v-for="(pack, i) in packs">
-                            <td class="level" :class="{ active: selectedPack === i }">
-                                <button @click="selectPack(i)">
-                                    <span class="type-label-lg">{{ pack.name }}</span>
-                                </button>
-                            </td>
-                        </tr>
-                    </table>
-                </div>
-
-                <div class="level-container">
-                    <div class="level" v-if="level">
-                        <h1>{{ level.name }}</h1>
-
-                        <p><b>CREATORS</b> {{ level.creators }}</p>
-                        <p><b>VERIFIER</b> {{ level.verifier }}</p>
-                        <p><b>PUBLISHER</b> {{ level.publisher }}</p>
-
-                        <iframe class="video" :src="level.video" frameborder="0"></iframe>
-                    </div>
-
-                    <div v-else class="level">
-                        <p>No level selected.</p>
-                    </div>
-                </div>
-
-                <div class="levels-container">
-                    <table class="list">
-                        <tr v-for="(lvl, i) in currentPack.levels">
-                            <td class="level" :class="{ active: selectedLevel === i }">
-                                <button @click="selectedLevel = i">
-                                    <span class="type-label-lg">{{ lvl.name }}</span>
-                                </button>
-                            </td>
-                        </tr>
-                    </table>
-                </div>
-
-            </div>
-        </main>
-    `,
-
-    computed: {
-        currentPack() {
-            return this.packs[this.selectedPack] || { levels: [] };
-        },
-        level() {
-            return this.currentPack.levels[this.selectedLevel];
-        }
-    },
-
-    methods: {
-        selectPack(i) {
-            this.selectedPack = i;
-            this.selectedLevel = 0;
-        }
-    },
-
-    async mounted() {
-        const packs = await fetchPacks();
-
-        if (!packs) {
-            this.err = "Failed to load packs.";
-        } else {
-            this.packs = packs;
-        }
-
-        this.loading = false;
-    }
-};
+[
+  {
+    "name": "Decoration I",
+    "levels": [
+      { "name": "envy" },
+      { "name": "spaceless" },
+      { "name": "Cookie Cat" },
+      { "name": "sog v2 jellicide" },
+      { "name": "Pink Hell" }
+    ]
+  },
+  {
+    "name": "sog pack I",
+    "levels": [
+      { "name": "sog v2 jellicide" },
+      { "name": "so soggy in here" },
+      { "name": "sgog channelg" }
+    ]
+  },
+  {
+    "name": "LGBTQ trilogy",
+    "levels": [
+      { "name": "trans circles" },
+      { "name": "bi circles" },
+      { "name": "lesbian circles" }
+    ]
+  },
+  {
+    "name": "Scary Scary Pack",
+    "levels": [
+      { "name": "scary scary gabe" },
+      { "name": "scary scary john" }
+    ]
+  },
+  {
+    "name": "Cube Slop Pack",
+    "levels": [
+      { "name": "sunshine" },
+      { "name": "help me up" },
+      { "name": "Moonlight" },
+      { "name": "scary scary gabe" },
+      { "name": "spaceless" }
+    ]
+  },
+  {
+    "name": "Learny Pack",
+    "levels": [
+      { "name": "overdose" },
+      { "name": "cube slop 8" },
+      { "name": "so soggy in here" }
+    ]
+  },
+  {
+    "name": "Easy Featured Pack I",
+    "levels": [
+      { "name": "scary scary gabe" },
+      { "name": "spaceless" }
+    ]
+  }
+]
