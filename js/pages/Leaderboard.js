bda6ff04b78644de6786a61ac3a880ae2830e696
scruffiebaka <itsmew2468@gmail.com>
2026-04-13 15:03:41 +0530

    fuckass

--- js/pages/Leaderboard.js
+++ js/pages/Leaderboard.js
@@ -6,15 +6,16 @@ export default {
     components: { Spinner },
 
     data: () => ({
-        mode: 'list',
-        loading: true,
+        mode: 'list', // 'list' or 'creator'
 
         leaderboard: [],
         creators: [],
 
+        loading: true,
         selected: 0,
-        err: null,
-        errList: []
+
+        err: [],
+        errCreators: null
     }),
 
     template: `
@@ -25,83 +26,136 @@ export default {
         <main v-else class="page-leaderboard-container">
             <div class="page-leaderboard">
 
-                <!-- TOGGLE -->
+                <!-- SELECTOR -->
                 <div class="toggle">
-                    <button 
-                        :class="{ active: mode === 'list' }"
-                        @click="switchMode('list')"
-                    >
-                        List Points
-                    </button>
-
-                    <button 
-                        :class="{ active: mode === 'creator' }"
-                        @click="switchMode('creator')"
-                    >
-                        Creator Points
-                    </button>
+                    <button @click="mode = 'list'; selected = 0">List Points</button>
+                    <button @click="mode = 'creator'; selected = 0">Creator Points</button>
                 </div>
 
-                <!-- ERROR -->
-                <p v-if="mode === 'creator' && err" class="error">{{ err }}</p>
-                <p v-if="mode === 'list' && errList.length" class="error">
-                    Leaderboard may be incorrect: {{ errList.join(', ') }}
-                </p>
+                <!-- LIST MODE -->
+                <template v-if="mode === 'list'">
 
-                <!-- LEFT LIST -->
-                <div class="board-container">
-                    <table class="board">
+                    <div class="error-container">
+                        <p class="error" v-if="err.length > 0">
+                            Leaderboard may be incorrect, as the following levels could not be loaded: {{ err.join(', ') }}
+                        </p>
+                    </div>
 
-                        <!-- LIST MODE -->
-                        <tr v-if="mode === 'list'"
-                            v-for="(e, i) in leaderboard"
-                        >
-                            <td class="type-label-lg" class="rank">#{{ i + 1 }}</td>
-                            <td class="type-label-lg" class="total">{{ localize(e.total) }}</td>
-                            <td class="type-label-lg" class="user" :class="{ active: selected === i }">
-                                <button @click="selected = i">
-                                    {{ e.user }}
-                                </button>
-                            </td>
-                        </tr>
+                    <div class="board-container">
+                        <table class="board">
+                            <tr 
+                                v-for="(ientry, i) in leaderboard"
+                                :class="{
+                                    'top-1': i === 0,
+                                    'top-2': i === 1,
+                                    'top-3': i === 2
+                                }"
+                            >
+                                <td class="rank">
+                                    <p class="type-label-lg">#{{ i + 1 }}</p>
+                                </td>
+                                <td class="total">
+                                    <p class="type-label-lg">{{ localize(ientry.total) }}</p>
+                                </td>
+                                <td class="user" :class="{ 'active': selected == i }">
+                                    <button @click="selected = i">
+                                        <span class="type-label-lg">{{ ientry.user }}</span>
+                                    </button>
+                                </td>
+                            </tr>
+                        </table>
+                    </div>
 
-                        <!-- CREATOR MODE -->
-                        <tr v-else
-                            v-for="(c, i) in creators"
-                        >
-                            <td class="type-label-lg" class="rank">#{{ i + 1 }}</td>
-                            <td class="type-label-lg" class="total">{{ c.points }}</td>
-                            <td class="type-label-lg" class="user" :class="{ active: selected === i }">
-                                <button @click="selected = i">
-                                    {{ c.user }}
-                                </button>
-                            </td>
-                        </tr>
-
-                    </table>
-                </div>
-
-                <!-- RIGHT PANEL -->
-                <div class="player-container">
-                    <div class="player">
-
-                        <!-- LIST DETAILS -->
-                        <template v-if="mode === 'list' && entry">
+                    <div class="player-container">
+                        <div class="player" v-if="entry">
                             <h1>#{{ selected + 1 }} {{ entry.user }}</h1>
                             <h3>{{ entry.total }}</h3>
-                        </template>
 
-                        <!-- CREATOR DETAILS -->
-                        <template v-if="mode === 'creator' && creator">
+                            <h2 v-if="entry.verified.length > 0">Verified ({{ entry.verified.length}})</h2>
+                            <table class="table">
+                                <tr v-for="score in entry.verified">
+                                    <td class="rank"><p>#{{ score.rank }}</p></td>
+                                    <td class="level">
+                                        <a class="type-label-lg" target="_blank" :href="score.link">{{ score.level }}</a>
+                                    </td>
+                                    <td class="score"><p>+{{ localize(score.score) }}</p></td>
+                                </tr>
+                            </table>
+
+                            <h2 v-if="entry.completed.length > 0">Completed ({{ entry.completed.length }})</h2>
+                            <table class="table">
+                                <tr v-for="score in entry.completed">
+                                    <td class="rank"><p>#{{ score.rank }}</p></td>
+                                    <td class="level">
+                                        <a class="type-label-lg" target="_blank" :href="score.link">{{ score.level }}</a>
+                                    </td>
+                                    <td class="score"><p>+{{ localize(score.score) }}</p></td>
+                                </tr>
+                            </table>
+
+                            <h2 v-if="entry.progressed.length > 0">Progressed ({{entry.progressed.length}})</h2>
+                            <table class="table">
+                                <tr v-for="score in entry.progressed">
+                                    <td class="rank"><p>#{{ score.rank }}</p></td>
+                                    <td class="level">
+                                        <a class="type-label-lg" target="_blank" :href="score.link">{{ score.percent }}% {{ score.level }}</a>
+                                    </td>
+                                    <td class="score"><p>+{{ localize(score.score) }}</p></td>
+                                </tr>
+                            </table>
+                        </div>
+                    </div>
+
+                </template>
+
+                <!-- CREATOR MODE -->
+                <template v-else>
+
+                    <p v-if="errCreators" class="error">{{ errCreators }}</p>
+
+                    <div class="board-container">
+                        <table class="board">
+                            <tr 
+                                v-for="(c, i) in creators"
+                                :class="{
+                                    'top-1': i === 0,
+                                    'top-2': i === 1,
+                                    'top-3': i === 2
+                                }"
+                            >
+                                <td class="rank">
+                                    <p>#{{ i + 1 }}</p>
+                                </td>
+                                <td class="total">
+                                    <p class="type-label-lg">{{ c.points }}</p>
+                                </td>
+                                <td class="user" :class="{ 'active': selected == i }">
+                                    <button @click="selected = i">
+                                        <span class="type-label-lg">{{ c.user }}</span>
+                                    </button>
+                                </td>
+                            </tr>
+                        </table>
+                    </div>
+
+                    <div class="player-container">
+                        <div class="player" v-if="creator">
                             <h1>#{{ selected + 1 }} {{ creator.user }}</h1>
                             <h3>{{ creator.points }} points</h3>
 
                             <h2>Best Awarded Level</h2>
                             <p>{{ creator.best }}</p>
-                        </template>
 
+                            <h2>Featured Levels ({{ creator.featured.length }})</h2>
+                            <table class="table">
+                                <tr v-for="lvl in creator.featured">
+                                    <td><p>{{ lvl }}</p></td>
+                                </tr>
+                            </table>
+                        </div>
                     </div>
-                </div>
+
+                </template>
 
             </div>
         </main>
@@ -118,21 +172,16 @@ export default {
 
     methods: {
         localize,
-
-        switchMode(m) {
-            this.mode = m;
-            this.selected = 0;
-        }
     },
 
     async mounted() {
         const [leaderboard, err] = await fetchLeaderboard();
         this.leaderboard = leaderboard;
-        this.errList = err;
+        this.err = err;
 
         const creators = await fetchCreators();
         if (!creators) {
-            this.err = "Failed to load creators.";
+            this.errCreators = "Failed to load creators.";
         } else {
             this.creators = creators.sort((a, b) => b.points - a.points);
         }
